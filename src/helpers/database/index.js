1cb45c8af5ca1a5dfbe7b09f1783519569eb5bbc
Jessi Leandro Castro <JLC@localhost.localdomain>
2019-10-07 11:52:01 -0300

    teste conection

--- src/helpers/database/index.js
+++ src/helpers/database/index.js
@@ -1,107 +1,17 @@
-const db = require('../../database')
-const TypeAccount = require('../../domains/auth/user/typeAccount')
-const UserDomain = require('../../domains/auth/user')
+const db = require("../../database");
 
-const typeAccount = new TypeAccount()
-const userDomain = new UserDomain()
+const dropAllTable = () => db.dropAllSchemas();
 
-const StockBase = db.model('stockBase')
+const isDatabaseConnected = () => db.authenticate();
 
-const dropAllTable = () => db.dropAllSchemas()
+const forceCreateTables = () =>
+  isDatabaseConnected().then(() => db.sync({ force: true }));
 
-const isDatabaseConnected = () => db
-  .authenticate()
-
-const forceCreateTables = () => isDatabaseConnected()
-  .then(() => db.sync({ force: true }))
-
-const createUserAdmin = async () => {
-  // const User = db.model('user')
-  // const Login = db.model('login')
-
-  const typeAccountMock = {
-    typeName: 'ADM2',
-    addCompany: true,
-    addPart: true,
-    addAnalyze: true,
-    addEquip: true,
-    addEntry: true,
-    addEquipType: true,
-    tecnico: true,
-    addAccessories: true,
-    addUser: true,
-    addTypeAccount: true,
-    responsibleUser: 'modrp',
-    stock: true,
-    labTec: true,
-    addTec: true,
-    addCar: true,
-    addMark: true,
-    addType: true,
-    addProd: true,
-    addFonr: true,
-    addEntr: true,
-    addKit: true,
-    addKitOut: true,
-    addOutPut: true,
-    addROs: true,
-    addRML: true,
-    gerROs: true,
-    delROs: true,
-    updateRos: true,
-  }
-  await typeAccount.add(typeAccountMock)
-
-  const userAdmin = {
-    username: 'modrp',
-    typeName: 'ADM2',
-    customized: false,
-    addCompany: true,
-    addPart: true,
-    addAnalyze: true,
-    addEquip: true,
-    addEntry: true,
-    addEquipType: true,
-    tecnico: true,
-    addAccessories: true,
-    addUser: true,
-    addTypeAccount: true,
-    // login: {
-    //   password: '102030',
-    // },
-    responsibleUser: 'modrp',
-    addTec: true,
-    addCar: true,
-    addMark: true,
-    addType: true,
-    addProd: true,
-    addFonr: true,
-    addEntr: true,
-    addKit: true,
-    addKitOut: true,
-    addOutPut: true,
-    addROs: true,
-    addRML: true,
-    gerROs: true,
-    delROs: true,
-    updateRos: true,
-  }
-
-  // await User.create(userAdmin, { include: [Login] })
-  await userDomain.user_Create(userAdmin)
-
-  await StockBase.create({ stockBase: 'REALPONTO' })
-  await StockBase.create({ stockBase: 'NOVAREAL' })
-  await StockBase.create({ stockBase: 'PONTOREAL' })
-}
-
-const dropAndDisconnectDatabase = () => db
-  .close()
+const dropAndDisconnectDatabase = () => db.close();
 
 module.exports = {
   isDatabaseConnected,
   forceCreateTables,
   dropAndDisconnectDatabase,
-  createUserAdmin,
-  dropAllTable,
-}
+  dropAllTable
+};
