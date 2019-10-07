781664cc9fd1318357fae7c465a454ca42a76e0e
Jessi Leandro Castro <JLC@localhost.localdomain>
2019-10-07 12:16:09 -0300

    teste get

--- src/server.js
+++ src/server.js
@@ -1,32 +1,43 @@
-require('./helpers/loadenv')
-const Express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const logger = require('morgan')
-const databaseHelper = require('./helpers/database')
-const errorFormatter = require('./helpers/errors/formatter')
-const loginRoute = require('./routes/login')
-const protectRoute = require('./routes/protect')
-const { auth } = require('./middlewares/authentic')
+require("./helpers/loadenv");
+const Express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const logger = require("morgan");
+const databaseHelper = require("./helpers/database");
+const errorFormatter = require("./helpers/errors/formatter");
+const loginRoute = require("./routes/login");
+const protectRoute = require("./routes/protect");
+const { auth } = require("./middlewares/authentic");
 
-const app = Express()
+const { Pool } = require("pg");
 
-// const authRoute = require('./routes/auth')
-// const unprotectedRoute = require('./routes/unprotected')
-// const authentication = require('./middlewares/authentication')
-// const uploadMiddleware = require('./middlewares/uploader')
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
-// const routes = require('./routes')
+const app = Express().get("/db", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM test_table");
+    const results = { results: result ? result.rows : null };
+    res.render("pages/db", results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send(`Error ${err}`);
+  }
+});
 
 /** MIDDLEWARES */
-app.use(logger('dev'))
-app.use(cors())
-app.use(Express.static('public'))
-app.use(bodyParser.json())
+app.use(logger("dev"));
+app.use(cors());
+app.use(Express.static("public"));
+app.use(bodyParser.json());
 
-app.use('/oapi', loginRoute)
+app.use("/oapi", loginRoute);
 // app.use('/api', middlewareAuthentic)
-app.use('/api', auth, protectRoute)
+app.use("/api", auth, protectRoute);
 // app.post('/contract-upload', uploadMiddleware('file', 'temporary', { isTemp: true }))
 
 // app.use(unprotectedRoute)
@@ -34,25 +45,23 @@ app.use('/api', auth, protectRoute)
 // app.use('/api', authRoute)
 // app.use('/api', routes)
 
-
 /* error handlers */
-app.use((err, req, res, next) => { //eslint-disable-line
+app.use((err, req, res, next) => {
+  //eslint-disable-line
 
   /* eslint-disable no-console */
-  console.error(err.stack || err)
-  console.error(JSON.stringify(err))
-  const formattedError = errorFormatter(err)
+  console.error(err.stack || err);
+  console.error(JSON.stringify(err));
+  const formattedError = errorFormatter(err);
 
-  res.status(formattedError.status || 500)
-  res.json(formattedError)
-})
+  res.status(formattedError.status || 500);
+  res.json(formattedError);
+});
 
-databaseHelper
-  .isDatabaseConnected()
-  .then(() => {
-    const { PORT } = process.env
+databaseHelper.isDatabaseConnected().then(() => {
+  const { PORT } = process.env;
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on ${PORT}`)
-    })
-  })
+  app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+  });
+});
