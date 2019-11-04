6bc0fab6513234d99f9b0f204877193b9e139a71
Jessi Leandro Castro <JLC@localhost.localdomain>
2019-11-04 11:38:30 -0300

    add Clint

---
+++ client/client.js
@@ -0,0 +1,51 @@
+const publicVapidKey =
+  "BFRJjJPwjqyWtcb2DfTK3E46btA1O7GzGfvYq54xU0YlnKi6LPBWaw6UnZikCNKdI--VKk-0ollDlzMqtqBiiew";
+
+if ("serviceWorker" in navigator) {
+  send().catch(err => console.error(err));
+}
+
+async function send() {
+  console.log("Registering service worker...");
+  const register = await navigator.serviceWorker.register("/sw.js", {
+    scope: "/"
+  });
+
+  console.log(register);
+
+  console.log("Service Worker Registered...");
+
+  console.log("Registering Push...");
+  const subscription = await register.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+  });
+
+  console.log("Push Registered...");
+
+  console.log("Send Push...");
+
+  // console.log(JSON.stringify(subscription));
+
+  await fetch("/subscribe", {
+    method: "POST",
+    body: JSON.stringify(subscription),
+    headers: {
+      "content-type": "application/json"
+    }
+  });
+  console.log("Push Sent....");
+}
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("./helpers/loadenv");
+const webpush = require("web-push");
 const Express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -15,11 +16,34 @@ const app = Express();
 /** MIDDLEWARES */
 app.use(logger("dev"));
 app.use(cors());
-app.use(Express.static("public"));
+app.use(Express.static("client"));
+// app.use(Express.static("public"));
 app.use(bodyParser.json());
-
 app.use("/oapi", loginRoute);
 app.use("/api", auth, protectRoute);
+
+const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
+const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+
+webpush.setVapidDetails(
+  "mailto:teste@test.com",
+  publicVapidKey,
+  privateVapidKey
+);
+
+app.post("/subscribe", (req, res) => {
+  const subscription = req.body;
+
+  // console.log(req);
+
+  res.status(201).json({});
+
+  const payload = JSON.stringify({ title: "Push Test" });
+
+  webpush
+    .sendNotification(subscription, payload)
+    .catch(err => console.log("sendNotification" + err));
+});
 
 /* error handlers */
 app.use((err, req, res, next) => {
@@ -33,6 +57,8 @@ app.use((err, req, res, next) => {
   res.status(formattedError.status || 500);
   res.json(formattedError);
 });
+
+// app.post("push-not", (req, res) => {});
 
 databaseHelper.isDatabaseConnected().then(() => {
   const { PORT } = process.env;
