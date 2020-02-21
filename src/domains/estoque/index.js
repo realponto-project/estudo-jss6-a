325bcebc22aade97da5d24c0e3b84c9f6a8aa2de
Jessi Leandro Castro <JLC@localhost.localdomain>
2020-02-21 15:33:24 -0300

    FIX: getAllEquips

--- src/domains/estoque/index.js
+++ src/domains/estoque/index.js
@@ -1,88 +1,84 @@
-const R = require('ramda')
+const R = require("ramda");
 // const moment = require('moment')
 
-const formatQuery = require('../../helpers/lazyLoad')
-const database = require('../../database')
+const formatQuery = require("../../helpers/lazyLoad");
+const database = require("../../database");
 
 // const { FieldValidationError } = require('../../helpers/errors')
 
 // const Mark = database.model('mark')
 // const Company = database.model('company')
 // const Entrance = database.model('entrance')
-const Product = database.model('product')
+const Product = database.model("product");
 // const User = database.model('user')
-const Mark = database.model('mark')
-const StockBase = database.model('stockBase')
-const ProductBase = database.model('productBase')
-const Notification = database.model('notification')
+const Mark = database.model("mark");
+const StockBase = database.model("stockBase");
+const ProductBase = database.model("productBase");
+const Notification = database.model("notification");
 // const Equip = database.model('equip')
 
 module.exports = class StockDomain {
   async getAll(options = {}) {
     const inicialOrder = {
-      field: 'createdAt',
+      field: "createdAt",
       acendent: true,
-      direction: 'DESC',
-    }
+      direction: "ASC"
+    };
 
-    const { query = null, transaction = null } = options
+    const { query = null, transaction = null } = options;
 
-    const newQuery = Object.assign({}, query)
-    const newOrder = query && query.order ? query.order : inicialOrder
+    const newQuery = Object.assign({}, query);
+    const newOrder = query && query.order ? query.order : inicialOrder;
 
     if (newOrder.acendent) {
-      newOrder.direction = 'DESC'
+      newOrder.direction = "DESC";
     } else {
-      newOrder.direction = 'ASC'
+      newOrder.direction = "ASC";
     }
 
-    const {
-      getWhere, limit, offset, pageResponse,
-    } = formatQuery(newQuery)
+    const { getWhere, limit, offset, pageResponse } = formatQuery(newQuery);
 
     const entrances = await ProductBase.findAndCountAll({
       // where: getWhere('productBase'),
-      attributes: ['id', 'amount', 'available'],
+      attributes: ["id", "amount", "available"],
       include: [
         {
           model: Product,
-          attributes: ['name', 'category', 'minimumStock'],
-          where: getWhere('product'),
+          attributes: ["name", "category", "minimumStock", "serial"],
+          where: getWhere("product"),
           // order: [
           //   ['name', 'ASC'],
           // ],
           include: [
             {
               model: Mark,
-              attributes: ['mark'],
-              required: true,
-            },
+              attributes: ["mark"],
+              required: true
+            }
           ],
-          required: true,
+          required: true
         },
         {
           model: StockBase,
-          attributes: ['stockBase'],
-          where: getWhere('stockBase'),
-        },
+          attributes: ["stockBase"],
+          where: getWhere("stockBase")
+        }
       ],
-      // order: [
-      //   [newOrder.field, newOrder.direction],
-      // ],
+      order: [[newOrder.field, newOrder.direction]],
       limit,
       offset,
-      transaction,
-    })
+      transaction
+    });
 
-    const { rows } = entrances
+    const { rows } = entrances;
 
     if (rows.length === 0) {
       return {
         page: null,
         show: 0,
         count: entrances.count,
-        rows: [],
-      }
+        rows: []
+      };
     }
 
     // const formatDateFunct = (date) => {
@@ -93,98 +89,99 @@ module.exports = class StockDomain {
     //   return dateformated
     // }
 
-    const formatData = R.map((entrance) => {
+    const formatData = R.map(entrance => {
       const resp = {
         id: entrance.id,
         amount: entrance.amount,
         available: entrance.available,
+        serial: entrance.product.serial,
         name: entrance.product.name,
         category: entrance.product.category,
         minimumStock: entrance.product.minimumStock,
         manufacturer: entrance.product.mark.mark,
-        stockBase: entrance.stockBase.stockBase,
-      }
-      return resp
-    })
+        stockBase: entrance.stockBase.stockBase
+      };
+      return resp;
+    });
 
-    const entrancesList = formatData(rows)
+    const entrancesList = formatData(rows);
 
-    let show = limit
+    let show = limit;
     if (entrances.count < show) {
-      show = entrances.count
+      show = entrances.count;
     }
 
     const response = {
       page: pageResponse,
       show,
       count: entrances.count,
-      rows: entrancesList,
-    }
+      rows: entrancesList
+    };
 
-    return response
+    return response;
   }
 
   async getAllNotification(options = {}) {
     const inicialOrder = {
-      field: 'createdAt',
+      field: "createdAt",
       acendent: true,
-      direction: 'DESC',
-    }
+      direction: "DESC"
+    };
 
-    const { query = null, transaction = null } = options
+    const { query = null, transaction = null } = options;
 
-    const newQuery = Object.assign({}, query)
-    const newOrder = query && query.order ? query.order : inicialOrder
+    const newQuery = Object.assign({}, query);
+    const newOrder = query && query.order ? query.order : inicialOrder;
 
     if (newOrder.acendent) {
-      newOrder.direction = 'DESC'
+      newOrder.direction = "DESC";
     } else {
-      newOrder.direction = 'ASC'
+      newOrder.direction = "ASC";
     }
 
-    const { limit, offset, pageResponse } = formatQuery(newQuery)
+    const { limit, offset, pageResponse } = formatQuery(newQuery);
 
     const notifications = await Notification.findAndCountAll({
       // attributes: ['id', 'amount', 'available'],
       order: [[newOrder.field, newOrder.direction]],
       limit,
       offset,
-      transaction,
-    })
+      transaction
+    });
 
-    const { rows } = notifications
+    const { rows } = notifications;
 
     if (rows.length === 0) {
       return {
         page: null,
         show: 0,
         count: notifications.count,
-        rows: [],
-      }
+        rows: []
+      };
     }
 
-    const formatData = R.map((entrance) => {
+    const formatData = R.map(entrance => {
       const resp = {
         id: entrance.id,
-        message: entrance.message,
-      }
-      return resp
-    })
+        message: entrance.message
+      };
+      return resp;
+    });
 
-    const notificationsList = formatData(rows)
+    const notificationsList = formatData(rows);
 
-    let show = limit
+    let show = limit;
     if (notifications.count < show) {
-      show = notifications.count
+      show = notifications.count;
     }
 
     const response = {
       page: pageResponse,
       show,
       count: notifications.count,
-      rows: notificationsList,
-    }
+      rows: notificationsList
+    };
 
-    return response
+    return response;
   }
-}
+};
