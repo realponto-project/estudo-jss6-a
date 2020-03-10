258ecc5f86e79baf1d2f7e14abbd40be893c9a51
Jessi Leandro Castro <JLC@localhost.localdomain>
2020-03-10 16:34:19 -0300

    getRInterno

--- src/controllers/estoque/reserve/index.js
+++ src/controllers/estoque/reserve/index.js
@@ -1,303 +1,334 @@
-const R = require('ramda')
+const R = require("ramda");
 
-const OsDomain = require('../../../domains/estoque/reserve/os')
-const KitDomain = require('../../../domains/estoque/reserve/kit')
-const KitOutDomain = require('../../../domains/estoque/reserve/kit/kitOut')
-const FreeMarketDomain = require('../../../domains/estoque/reserve/freeMarket')
-const TechnicianReserveDomain = require('../../../domains/estoque/reserve/technician')
-const StatusExpeditionDomain = require('../../../domains/estoque/reserve/os/statusExpedition')
-const database = require('../../../database')
+const OsDomain = require("../../../domains/estoque/reserve/os");
+const KitDomain = require("../../../domains/estoque/reserve/kit");
+const KitOutDomain = require("../../../domains/estoque/reserve/kit/kitOut");
+const FreeMarketDomain = require("../../../domains/estoque/reserve/freeMarket");
+const TechnicianReserveDomain = require("../../../domains/estoque/reserve/technician");
+const StatusExpeditionDomain = require("../../../domains/estoque/reserve/os/statusExpedition");
+const database = require("../../../database");
 
-const osDomain = new OsDomain()
-const kitDomain = new KitDomain()
-const kitOutDomain = new KitOutDomain()
-const freeMarketDomain = new FreeMarketDomain()
-const technicianReserveDomain = new TechnicianReserveDomain()
-const statusExpeditionDomain = new StatusExpeditionDomain()
+const osDomain = new OsDomain();
+const kitDomain = new KitDomain();
+const kitOutDomain = new KitOutDomain();
+const freeMarketDomain = new FreeMarketDomain();
+const technicianReserveDomain = new TechnicianReserveDomain();
+const statusExpeditionDomain = new StatusExpeditionDomain();
 
 const addRInterno = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    const technicianReserve = await technicianReserveDomain.add(req.body, { transaction })
+    const technicianReserve = await technicianReserveDomain.add(req.body, {
+      transaction
+    });
 
-    await transaction.commit()
-    res.json(technicianReserve)
+    await transaction.commit();
+    res.json(technicianReserve);
   } catch (error) {
-    await transaction.rollback()
-    next(error)
+    await transaction.rollback();
+    next(error);
   }
-}
+};
+
+const getRInterno = async (req, res, next) => {
+  const transaction = await database.transaction();
+  try {
+    let query;
+    if (R.has("query", req)) {
+      if (R.has("query", req.query)) {
+        query = JSON.parse(req.query.query);
+      }
+    }
+    const technicianReserves = await technicianReserveDomain.getAll({
+      query,
+      transaction
+    });
+
+    await transaction.commit();
+    res.json(technicianReserves);
+  } catch (error) {
+    await transaction.rollback();
+    next(error);
+  }
+};
 
 const addOs = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    const Os = await osDomain.add(req.body, { transaction })
+    const Os = await osDomain.add(req.body, { transaction });
 
-    await transaction.commit()
-    res.json(Os)
+    await transaction.commit();
+    res.json(Os);
   } catch (error) {
-    await transaction.rollback()
-    next(error)
+    await transaction.rollback();
+    next(error);
   }
-}
+};
 
 const updateOs = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    const Os = await osDomain.update(req.body, { transaction })
+    const Os = await osDomain.update(req.body, { transaction });
 
-    await transaction.commit()
-    res.json(Os)
+    await transaction.commit();
+    res.json(Os);
   } catch (error) {
-    await transaction.rollback()
-    next(error)
+    await transaction.rollback();
+    next(error);
   }
-}
+};
 
 const output = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    const Os = await osDomain.output(req.body, { transaction })
+    const Os = await osDomain.output(req.body, { transaction });
 
-    await transaction.commit()
-    res.json(Os)
+    await transaction.commit();
+    res.json(Os);
   } catch (error) {
-    await transaction.rollback()
-    next(error)
+    await transaction.rollback();
+    next(error);
   }
-}
+};
 
 const deleteOs = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    const Os = await osDomain.delete(req.query.osId, { transaction })
+    const Os = await osDomain.delete(req.query.osId, { transaction });
 
-    await transaction.commit()
-    res.json(Os)
+    await transaction.commit();
+    res.json(Os);
   } catch (error) {
-    await transaction.rollback()
-    next(error)
+    await transaction.rollback();
+    next(error);
   }
-}
+};
 
 const addKit = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    const kitOut = await kitDomain.add(req.body, { transaction })
+    const kitOut = await kitDomain.add(req.body, { transaction });
 
-    await transaction.commit()
-    res.json(kitOut)
+    await transaction.commit();
+    res.json(kitOut);
   } catch (error) {
-    await transaction.rollback()
-    next(error)
+    await transaction.rollback();
+    next(error);
   }
-}
+};
 
 const getAllKit = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    let query
-    if (R.has('query', req)) {
-      if (R.has('query', req.query)) {
-        query = JSON.parse(req.query.query)
+    let query;
+    if (R.has("query", req)) {
+      if (R.has("query", req.query)) {
+        query = JSON.parse(req.query.query);
       }
     }
 
-    const kits = await kitDomain.getAll({ query, transaction })
+    const kits = await kitDomain.getAll({ query, transaction });
 
-
-    await transaction.commit()
-    res.json(kits)
+    await transaction.commit();
+    res.json(kits);
   } catch (error) {
-    await transaction.rollback()
-    next()
+    await transaction.rollback();
+    next();
   }
-}
+};
 
 const getKitDefaultValue = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    let query
-    if (R.has('query', req)) {
-      if (R.has('query', req.query)) {
-        query = JSON.parse(req.query.query)
+    let query;
+    if (R.has("query", req)) {
+      if (R.has("query", req.query)) {
+        query = JSON.parse(req.query.query);
       }
     }
 
-    const kitDefault = await kitDomain.getKitDefaultValue({ query, transaction })
+    const kitDefault = await kitDomain.getKitDefaultValue({
+      query,
+      transaction
+    });
 
-
-    await transaction.commit()
-    res.json(kitDefault)
+    await transaction.commit();
+    res.json(kitDefault);
   } catch (error) {
-    await transaction.rollback()
-    next()
+    await transaction.rollback();
+    next();
   }
-}
+};
 
 const addKitOut = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    const kitOut = await kitOutDomain.add(req.body, { transaction })
+    const kitOut = await kitOutDomain.add(req.body, { transaction });
 
-    await transaction.commit()
-    res.json(kitOut)
+    await transaction.commit();
+    res.json(kitOut);
   } catch (error) {
-    await transaction.rollback()
-    next(error)
+    await transaction.rollback();
+    next(error);
   }
-}
+};
 
 const getAllKitOut = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    let query
-    if (R.has('query', req)) {
-      if (R.has('query', req.query)) {
-        query = JSON.parse(req.query.query)
+    let query;
+    if (R.has("query", req)) {
+      if (R.has("query", req.query)) {
+        query = JSON.parse(req.query.query);
       }
     }
 
-    const kitsOut = await kitOutDomain.getAll({ query, transaction })
+    const kitsOut = await kitOutDomain.getAll({ query, transaction });
 
-    await transaction.commit()
-    res.json(kitsOut)
+    await transaction.commit();
+    res.json(kitsOut);
   } catch (error) {
-    await transaction.rollback()
-    next()
+    await transaction.rollback();
+    next();
   }
-}
+};
 
 const addFreeMarket = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    const freeMarket = await freeMarketDomain.add(req.body, { transaction })
+    const freeMarket = await freeMarketDomain.add(req.body, { transaction });
 
-    await transaction.commit()
-    res.json(freeMarket)
+    await transaction.commit();
+    res.json(freeMarket);
   } catch (error) {
-    await transaction.rollback()
-    next(error)
+    await transaction.rollback();
+    next(error);
   }
-}
+};
 
 const getAllFreeMarket = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    let query
-    if (R.has('query', req)) {
-      if (R.has('query', req.query)) {
-        query = JSON.parse(req.query.query)
+    let query;
+    if (R.has("query", req)) {
+      if (R.has("query", req.query)) {
+        query = JSON.parse(req.query.query);
       }
     }
 
-    const FreeMarket = await freeMarketDomain.getAll({ query, transaction })
+    const FreeMarket = await freeMarketDomain.getAll({ query, transaction });
 
-
-    await transaction.commit()
-    res.json(FreeMarket)
+    await transaction.commit();
+    res.json(FreeMarket);
   } catch (error) {
-    await transaction.rollback()
-    next()
+    await transaction.rollback();
+    next();
   }
-}
+};
 
 const getAllOs = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    let query
-    if (R.has('query', req)) {
-      if (R.has('query', req.query)) {
-        query = JSON.parse(req.query.query)
+    let query;
+    if (R.has("query", req)) {
+      if (R.has("query", req.query)) {
+        query = JSON.parse(req.query.query);
       }
     }
 
-    const entrances = await osDomain.getAll({ query, transaction })
+    const entrances = await osDomain.getAll({ query, transaction });
 
-    await transaction.commit()
-    res.json(entrances)
+    await transaction.commit();
+    res.json(entrances);
   } catch (error) {
-    await transaction.rollback()
-    next()
+    await transaction.rollback();
+    next();
   }
-}
+};
 
 const getOsByOs = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    const { os } = req.query
-    const company = await osDomain.getOsByOs(os)
+    const { os } = req.query;
+    const company = await osDomain.getOsByOs(os);
 
-    await transaction.commit()
-    res.json(company)
+    await transaction.commit();
+    res.json(company);
   } catch (error) {
-    await transaction.rollback()
-    next()
+    await transaction.rollback();
+    next();
   }
-}
+};
 
 const addStatusExpedition = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    const status = await statusExpeditionDomain.add(req.body, { transaction })
+    const status = await statusExpeditionDomain.add(req.body, { transaction });
 
-    await transaction.commit()
-    res.json(status)
+    await transaction.commit();
+    res.json(status);
   } catch (error) {
-    await transaction.rollback()
-    next(error)
+    await transaction.rollback();
+    next(error);
   }
-}
+};
 
 const updateStatusExpedition = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    const status = await statusExpeditionDomain.update(req.body, { transaction })
+    const status = await statusExpeditionDomain.update(req.body, {
+      transaction
+    });
 
-    await transaction.commit()
-    res.json(status)
+    await transaction.commit();
+    res.json(status);
   } catch (error) {
-    await transaction.rollback()
-    next(error)
+    await transaction.rollback();
+    next(error);
   }
-}
-
+};
 
 const deleteStatusExpedition = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    const status = await statusExpeditionDomain.delete(req.query.id, { transaction })
+    const status = await statusExpeditionDomain.delete(req.query.id, {
+      transaction
+    });
 
-    await transaction.commit()
-    res.json(status)
+    await transaction.commit();
+    res.json(status);
   } catch (error) {
-    await transaction.rollback()
-    next(error)
+    await transaction.rollback();
+    next(error);
   }
-}
+};
 
 const getAllStatusExpedition = async (req, res, next) => {
-  const transaction = await database.transaction()
+  const transaction = await database.transaction();
   try {
-    let query
-    if (R.has('query', req)) {
-      if (R.has('query', req.query)) {
-        query = JSON.parse(req.query.query)
+    let query;
+    if (R.has("query", req)) {
+      if (R.has("query", req.query)) {
+        query = JSON.parse(req.query.query);
       }
     }
 
-    const entrances = await statusExpeditionDomain.getAll({ query, transaction })
+    const entrances = await statusExpeditionDomain.getAll({
+      query,
+      transaction
+    });
 
-    await transaction.commit()
-    res.json(entrances)
+    await transaction.commit();
+    res.json(entrances);
   } catch (error) {
-    await transaction.rollback()
-    next()
+    await transaction.rollback();
+    next();
   }
-}
+};
 
 module.exports = {
   addRInterno,
+  getRInterno,
   addOs,
   output,
   updateOs,
@@ -314,6 +345,6 @@ module.exports = {
   addStatusExpedition,
   updateStatusExpedition,
   deleteStatusExpedition,
-  getAllStatusExpedition,
+  getAllStatusExpedition
   //   getAll,
-}
+};
